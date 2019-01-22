9ce5706378b47ce11ce560ab6d13a545e5df064c
novandev <donovan.adams@students.makeschool.com>
2019-01-22 14:58:41 -0800

    addding more to the repo

---
+++ touchOfGo1.go
@@ -0,0 +1,12 @@
+package main
+
+//Importing modules fmt and time to count time
+import (
+	"fmt"
+	"time"
+)
+
+func main(){
+	fmt.Println("Welcome to the playground")
+	fmt.Println("the time is ",time.Now())
+}
